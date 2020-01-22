3b943aa88c4db1860a5f9b3453c346960aa71c3e
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-01-23 01:37:01 +0600

    Delete Text recognition.py

--- Text recognition.py
+++
@@ -1,6 +0,0 @@
-import pytesseract
-from PIL import Image
-img = Image.open('F:/Photos/Campus life(Tangail)/20190727_105517.jpg')
-pytesseract.pytesseract.tesseract_cmd = r'D:\Softwares and apps\tesseract.exe'
-text = pytesseract.image_to_string(img, config='')
-print(text)
