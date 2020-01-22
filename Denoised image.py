55fa608f27b97c905dc1b6018444e45a7b532459
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-01-23 02:11:07 +0600

    Delete Denoised image.py

--- Denoised image.py
+++
@@ -1,8 +0,0 @@
-import cv2
-img = cv2.imread("F:/Photos/Campus life(Tangail)/WP_20151012_14_40_59_Pro.jpg")
-result = cv2.fastNlMeansDenoisingColored(img,None,20,10,7,21)
-cv2.imshow("Original Image", img)
-
-cv2.imshow("Denoised Image", result)
-
-cv2.waitKey(0)
