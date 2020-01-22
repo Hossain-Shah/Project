5948783254f191840c8f79301dbabe68076739b0
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-01-23 01:41:04 +0600

    Delete Semantic net.py

--- Semantic net.py
+++
@@ -1,8 +0,0 @@
-from pattern.graph import Graph
-g = Graph()
-g.add_edge('doll', 'toy', type='is-a')
-g.add_edge('silent', 'doll', type='is-property-of')
-g.add_edge('doll', 'girl', type='is-related-to')
-node = g['doll']
-print(node.id)
-print(node.links)
