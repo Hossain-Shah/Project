2c22b50749b3c9a41c8cb5212132b56150a4a949
Md Shahnawaz Hossain <30951203+Hossain-Shah@users.noreply.github.com>
2020-06-12 22:31:50 +0600

    Add files via upload

---
+++ AI & ML/Template matching.py
@@ -0,0 +1,22 @@
+import re
+import cv2
+import pytesseract
+from pytesseract import Output
+img = cv2.imread('invoice-sample.jpg')
+d = pytesseract.image_to_data(img, output_type=Output.DICT)
+keys = list(d.keys())
+date_pattern = '^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[012])/(19|20)\d\d$'
+n_boxes = len(d['text'])
+for i in range(n_boxes):
+    if int(d['conf'][i]) > 60:
+        if re.match(date_pattern, d['text'][i]):
+            (x, y, w, h) = (d['left'][i], d['top'][i], d['width'][i], d['height'][i])
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+cv2.imshow('img', img)
+cv2.waitKey(0)
+custom_config = r'--oem 3 --psm 6 outputbase digits'
+print(pytesseract.image_to_string(img, config=custom_config))
+custom_config = r'-c tessedit_char_whitelist=abcdefghijklmnopqrstuvwxyz --psm 6'
+print(pytesseract.image_to_string(img, config=custom_config))
+custom_config = r'-c tessedit_char_blacklist=0123456789 --psm 6'
+print(pytesseract.image_to_string(img, config=custom_config))
