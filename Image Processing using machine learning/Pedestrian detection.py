f6a310dbee4a0f8bd0ec0a50e0e3a28321f02dc7
Md Shahnawaz Hossain <shahnawazhossain07@gmail.com>
2021-04-23 18:19:41 +0600

    Update Pedestrian detection.py

--- Image Processing using machine learning/Pedestrian detection.py
+++ Image Processing using machine learning/Pedestrian detection.py
@@ -5,8 +5,13 @@ hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 image = cv2.imread('F:/Photos/Campus life(Tangail)/SAM_3005.JPG')
 image = imutils.resize(image, width=min(400, image.shape[1]))
 (regions, _) = hog.detectMultiScale(image, winStride=(4, 4), padding=(4, 4), scale=1.05)
+person = 1
 for (x, y, w, h) in regions:
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.putText(image, f'person {person}', (x, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1)
+    person += 1
+    cv2.putText(image, 'Status : Detecting ', (40, 40), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
+    cv2.putText(image, f'Total Persons : {person - 1}', (40, 70), cv2.FONT_HERSHEY_DUPLEX, 0.8, (255, 0, 0), 2)
 cv2.imshow("Pedestrian detected", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
