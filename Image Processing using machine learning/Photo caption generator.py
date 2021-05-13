baad531e2c493563bcf4dcd411d1f2638d8fe356
Md Shahnawaz Hossain <shahnawazhossain07@gmail.com>
2021-05-14 01:37:40 +0600

    Update Photo caption generator.py

--- Image Processing using machine learning/Photo caption generator.py
+++ Image Processing using machine learning/Photo caption generator.py
@@ -1,86 +1,63 @@
-import string
-filename = "Flickr8k.token.txt"
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.applications.xception import Xception
+from keras.models import load_model
+from pickle import load
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
 
 
-def load_doc(filename):
-    file = open(filename, 'r')
-    text = file.read()
-    file.close()
-    return text
+def extract_features(filename, model):
+    try:
+        image = Image.open(filename)
+
+    except:
+        print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
+    image = image.resize((299, 299))
+    image = np.array(image)
+    # for images that has 4 channels, we convert them into 3 channels
+    if image.shape[2] == 4:
+        image = image[..., :3]
+    image = np.expand_dims(image, axis=0)
+    image = image / 127.5
+    image = image - 1.0
+    feature = model.predict(image)
+    return feature
 
 
-doc = load_doc(filename)
-print(doc[:300])
+def word_for_id(integer, tokenizer):
+ for word, index in tokenizer.word_index.items():
+     if index == integer:
+         return word
+ return None
 
 
-def load_descriptions(doc):
-    mapping = dict()
-    for line in doc.split('\n'):
-        tokens = line.split()
-        if len(line) < 2:
-            continue
-        image_id, image_desc = tokens[0], tokens[1:]
-        image_id = image_id.split('.')[0]
-        image_desc = ' '.join(image_desc)
-        if image_id not in mapping:
-            mapping[image_id] = list()
-            mapping[image_id].append(image_desc)
-            return mapping
+def generate_desc(model, tokenizer, photo, max_length):
+    in_text = 'start'
+    for i in range(max_length):
+        sequence = tokenizer.texts_to_sequences([in_text])[0]
+        sequence = pad_sequences([sequence], maxlen=max_length)
+        pred = model.predict([photo, sequence], verbose=0)
+        pred = np.argmax(pred)
+        word = word_for_id(pred, tokenizer)
+        if word is None:
+            break
+        in_text += ' ' + word
+        if word == 'end':
+            break
+    return in_text
 
 
-descriptions = load_descriptions(doc)
-print('Loaded: %d ' % len(descriptions))
-
-
-def clean_descriptions(descriptions):
-    table = str.maketrans('', '', string.punctuation)
-    for key, desc_list in descriptions.items():
-        for i in range(len(desc_list)):
-            desc = desc_list[i]
-            desc = desc.split()
-            desc = [word.lower() for word in desc]
-            desc = [w.translate(table) for w in desc]
-            desc = [word for word in desc if len(word)>1]
-            desc = [word for word in desc if word.isalpha()]
-            desc_list[i] =  ' '.join(desc)
-
-
-def to_vocabulary(descriptions):
-    all_desc = set()
-    for key in descriptions.keys():
-        [all_desc.update(d.split()) for d in descriptions[key]]
-        return all_desc
-
-
-vocabulary = to_vocabulary(descriptions)
-print('Original Vocabulary Size: %d' % len(vocabulary))
-
-
-def save_descriptions(descriptions, filename):
-    lines = list()
-    for key, desc_list in descriptions.items():
-        for desc in desc_list:
-            lines.append(key + ' ' + desc)
-            data = '\n'.join(lines)
-            file = open(filename, 'w')
-            file.write(data)
-            file.close()
-
-
-save_descriptions(descriptions, 'descriptions.txt')
-
-
-def load_set(filename):
-    doc = load_doc(filename)
-    dataset = list()
-    for line in doc.split('\n'):
-        if len(line) < 1:
-            continue
-        identifier = line.split('.')[0]
-        dataset.append(identifier)
-        return set(dataset)
-
-
-filename = 'Flickr_8k.trainImages.txt'
-train = load_set(filename)
-print('Dataset: %d' % len(train))
+max_length = 32
+model = load_model('captionmodels/model_9.h5')
+xception_model = Xception(include_top=False, pooling="avg")
+img_path = "F:/Photos/Campus life(Tangail)/SAM_3005.jpg"
+img = Image.open(img_path)
+tokenizer = load(open("captiontokenizer.p", "rb"))
+photo = extract_features(img_path, xception_model)
+description = generate_desc(model, tokenizer, photo, max_length)
+print("\n\n")
+print(description)
+plt.imshow(img)
+plt.show()
