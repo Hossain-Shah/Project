0ff747c3d6e6748ebb2272cb3ec3cb850601287b
Md Shahnawaz Hossain <shahnawazhossain07@gmail.com>
2021-07-19 16:45:37 +0600

    Update Text recognition.py

--- AI & ML/Text recognition.py
+++ AI & ML/Text recognition.py
@@ -1,6 +1,8 @@
 import pytesseract
 from PIL import Image
+from langdetect import detect_langs
 img = Image.open('F:/Photos/Campus life(Tangail)/20190727_105517.jpg')
 pytesseract.pytesseract.tesseract_cmd = r'D:\Softwares and apps\tesseract.exe'
 text = pytesseract.image_to_string(img, config='')
 print(text)
+print(detect_langs(text))
